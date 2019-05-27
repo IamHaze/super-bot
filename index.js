fff892623b99a69e8c295dbaf1a11596bdc63dcb
IamHaze <masterhaze93@yahoo.com>
2019-05-27 08:37:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const bot = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am Alive!');
-});
+const token = 'NTgxNDA2ODUxMjA0MTIwNTg2.XOiARg.eej2yJBUZ-AKlqG833Ns-9ebayg';
 
-client.on('ready' , () =>{
+const PREFIX = '!';
+
+bot.on('ready' , () =>{
     console.log('Im Alive')
-    client.user.setActivity('You Human', {type : "WATCHING"}).catch(console.error);
 })
 
-client.on('guildMemberAdd', member =>{
+bot.on('guildMemberAdd', member =>{
 
     const channel = member.guild.find(channel => channel.name === "general");
     if(!channel) return;
@@ -18,7 +17,10 @@ client.on('guildMemberAdd', member =>{
     channel.send(`Welcome Human to MasteR HazE Server, ${member}, Do Whatever u want here!`)
 })
 
-client.on('message' , message=>{
+
+
+
+bot.on('message' , message=>{
   
     let args = message.content.substring(PREFIX.length).split(" ");
 
@@ -39,20 +41,15 @@ client.on('message' , message=>{
         case 'fb' :
              message.reply('@masterhaze93');
             break; 
-        case 'hi' :
+         case 'hi' :
              message.reply('Hello Fellow Human');
-        case 'bot' :
-             message.reply('You Summon me Human.?');
-        case 'bye' :
-             message.reply('Goodbye Human!');
-        break;
+            break; 
         case 'clear':
         if(!args[1]) return message.reply('Error Command Not Found =.=!')   
         message.channel.bulkDelete(args[1]);
         break;
     }
 })
-});
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
