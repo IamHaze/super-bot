94fe1fbf12dacd41bd6816b376dbfa9965dccc44
IamHaze <masterhaze93@yahoo.com>
2019-10-08 14:45:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -65,9 +65,6 @@ bot.on('message' , message=>{
         case 'info-jail' :
              message.reply('Boss blh baca dkt #jail-role sana ada semua info Boss');
         break;
-        case 'boss' :
-             message.reply('@!316204134879592448 TEST');
-        break;
     }
 })
 // THIS  MUST  BE  THIS  WAY
