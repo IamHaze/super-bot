30062c33b47329753c9b7815efc9e86c2061b939
IamHaze <masterhaze93@yahoo.com>
2020-03-06 21:22:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ bot.on('message' , message=>{
             message.reply('Nk apa Bossku?');
             break;
         case 'owner' :
-            message.reply('@OptimusPrimeXxx#1374 Dia ni paling hebat');
+            message.reply('@329159703525326858 Dia ni paling hebat');
             break;
         case 'warning' :
-            message.reply('@Haziq29#7494 Bosskita ni jgn kacau dia');
+            message.reply('@242528380719726594 Bosskita ni jgn kacau dia');
             break;
         case 'welcome':
             if(args[1] == 'Welcome To MUGG Server'){
@@ -55,16 +55,11 @@ bot.on('message' , message=>{
         case 'bye' :
              message.reply('Goodbye Human!');
         break;
-        case 'cikgu-rp' :
-             message.reply('Bosskita ni tukang ajar RP @Mugi @AbeAmir Dgr Baik2!');
-            break;
         case 'clear':
         if(!args[1]) return message.reply('Nk clear berapa ni boss =.=!')   
         message.channel.bulkDelete(args[1]);
         break;
-        case 'info-jail' :
-             message.reply('Boss blh baca dkt #jail-role sana ada semua info Boss');
-        break;
+        
     }
 })
 // THIS  MUST  BE  THIS  WAY
