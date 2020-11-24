70350fefc08d6646baac413e5b5a33154c8d5925
IamHaze <masterhaze93@yahoo.com>
2020-11-24 21:08:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ bot.on('message' , message=>{
             break;
         case 'welcome':
             if(args[1] == 'newbie'){
-            message.reply('Selamat Datang ke  WORP Server Bg new player silakan baca segala peraturan server kami. Terima Kasih ');
+            message.reply('Selamat Datang ke Server Bg new player silakan baca segala peraturan server kami. Terima Kasih ');
             }else{
                 message.reply('Invalid Command')
             }
